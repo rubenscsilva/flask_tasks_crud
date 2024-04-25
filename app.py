48615e1e32ea0376c550e83b672b6d2ad5ec972d
rubenscsilva <rubens_nett@yahoo.com.br>
2024-04-25 00:23:50 -0300

    feat: GET tasks

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
-from flask import flask
+from flask import Flask, request, jsonify
+from models.tasks import Task
 
+app = Flask(__name__)
+
+#CRUD : Create, Read, Update and Delete
+
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def created_task():
+    global task_id_control
+    data = request.get_json()
+    # new_task = Task(id=task_id_control, title=data.get('title'), description=data.get('description', ''))
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ''))
+    task_id_control +=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({'message': 'Nova tarefa criada com sucesso'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
